fcd6a6914851842bd44ab583545f0dd0ec2817bd
Bozhidar00 <147531776+Bozhidar00@users.noreply.github.com>
2023-10-21 23:21:06 +0300

    Update toy_store.py

--- toy_store.py
+++ toy_store.py
@@ -1,5 +1,3 @@
-from typing import Any
-
 puzzle = 2.60
 puppet = 3
 teddy = 4.10
@@ -13,18 +11,20 @@ teddies = float(input())
 minions = float(input())
 trucks = float(input())
 
-percent_off = {}
-price = (puzzles * puzzle) * (puppets * puppet) * (teddies * teddy) * (minions * minion) * (trucks * truck)
+price = (puzzles * puzzle) + (puppets * puppet) + (teddies * teddy) + (minions * minion) + (trucks * truck)
 
 delivery_quantity = puzzles + puppets + teddies + minions + trucks
 if delivery_quantity >= 50:
-    percent_off = (price * 25 / 100)
+    price -= price * 0.25
+    rent = price * 0.10
+else:
+    rent = price * 0.10
 
-final_price = (price + percent_off)
+final_price = price - rent
 
 if final_price >= trip:
     lv_left = final_price - trip
-    print(f"Yes! {lv_left} lv left ")
+    print(f"Yes! {lv_left:.2f} lv left")
 else:
     lv_not_enough = trip - final_price
-    f"not enough money! {lv_not_enough}lv needed."
+    print(f"Not enough money! {lv_not_enough:.2f} lv needed.")
